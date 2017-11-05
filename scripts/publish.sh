9ca10e3a75dff147b7a678f3dd4c4d7d5ca7747b
Adam Abonyi <abonyi@seznam.cz>
2017-11-05 21:43:18 +0100

    add all files to commit in publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
-
+ 
 git checkout -b "$1"
 git merge master
 sed -i -e 's/dist/tralala\//g' .gitignore;
-cat .gitignore
+git add -A 
 git commit -am "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
 git push --force --set-upstream origin $1 
