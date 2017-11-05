808a1595b456e36347c2575b9fd3d2bd85bd495f
Adam Abonyi <abonyi@seznam.cz>
2017-11-05 21:34:22 +0100

    Publishing script change

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,5 +5,6 @@ git config --global user.name "$USER_NAME"
 git checkout -b "$1"
 git merge master
 sed -i -e 's/dist/tralala\//g' .gitignore;
+cat .gitignore
 git commit -am "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
 git push --force --set-upstream origin $1 
