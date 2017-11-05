8219fdb7ef6638a82f13b29020bff19c3d624556
Adam Abonyi <abonyi@seznam.cz>
2017-11-05 21:57:19 +0100

    Publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 git config --global user.email "abonyi@seznam.cz"
 git config --global user.name "AdamAbonyi"
+git config --global push.default matching
  
 git checkout -b "$1"
 git merge master
 sed -i -e 's/dist/tralala\//g' .gitignore;
 git add -A 
-git status
 git commit -am "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
-git push --force 
+git status
+git push --force
